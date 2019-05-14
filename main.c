06e3a2af2bbc116b2da9c518832fd2b142d6534a
popwser <michaelbuergerart@gmail.com>
2019-05-13 21:07:10 -0700

    Updated mode picking to allow properties beyond mode (->program properties)

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-const char *getStartMode()
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+struct ProgramProperties
+{
+  char startMode[6];
+  // TODO: Add IP and port that server starts on
+};
+
+struct ProgramProperties* getProgramProperties()
 {
   char inputModeStr[6];
 
@@ -11,23 +22,41 @@ const char *getStartMode()
   if(!strcmp(inputModeStr, "client"))
   {
     printf("Starting in %s mode...\n", "client");
-    return "client";
+
+    struct ProgramProperties* prop = malloc(sizeof(struct ProgramProperties));
+    strcpy(prop->startMode, "client");
+
+    return prop;
   }
   if(!strcmp(inputModeStr, "server"))
   {
     printf("Starting in %s mode...\n", "server");
-    return "server";
+
+    struct ProgramProperties* prop = malloc(sizeof(struct ProgramProperties));
+    strcpy(prop->startMode, "server");
+
+    return prop;
   }
 
   printf("\"%s\" is not a valid mode, please try one of the following:\n\"client\", \"server\"\n", inputModeStr);
-  return getStartMode();
+  return getProgramProperties();
 }
 
 int main()
 {
 
-  const char *startMode = getStartMode();
-  // Do other stuff
+  struct ProgramProperties* prop = getProgramProperties();
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP, IP
+
+  if(!strcmp(prop->startMode, "client"))
+  {}
+
+  if(!strcmp(prop->startMode, "server"))
+  {}
+
+  free(prop->startMode);
+  free(prop);
 
   return 0;
 }
